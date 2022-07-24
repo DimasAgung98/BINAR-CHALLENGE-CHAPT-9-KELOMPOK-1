34d2c9cd80029455d439a1b98abaab4a1fa4db02
dimas agung <dimasraden0798@gmail.com>
2022-07-24 21:55:35 +0800

    [Dimas] add multer to server

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -2,19 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 
-// //PENGATURAN STORAGE UNTUK MULTER
-// const myStorage = multer.diskStorage({
-//     destination: './public/uploads',
-//     filename: (req, file, callback) => {
-//         callback(null, file.originalname); // upload dengan nama asli
-//     }
-// });
-
-// // middleware untuk upload dengan multer
-// const upload = multer({ storage: myStorage });
-
 const uploadPicture = async (req, res) => {
-    // upload.single('photo')
     let imageURL = req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename;
     console.log(req.file);
     console.log(req.body);
